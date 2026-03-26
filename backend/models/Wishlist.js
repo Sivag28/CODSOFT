0f816f6d08ebc1939ea3b94bc3b6a7fa208a62fc
Sivag28 <siva.g282005@gmail.com>
2026-03-26 11:16:30 +0530

    Remove old structure

--- backend/models/Wishlist.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export default mongoose.model(
-  "Wishlist",
-  new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-  })
-);
